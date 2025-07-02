166e185b09edb5cfee289a1775e61d9c60b6529d
abnerLouren <abnerjaredejede@gmail.com>
2025-07-02 18:48:08 +0100

    definindo estrutura de diretorios

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+function Sidebar({}) {
+  return (
+    <>
+      <nav className="bg-black text-white flex">
+        <div>
+          <img src="" alt="logo" />
+        </div>
+        <ul className="flex">
+          <li>Home</li>
+          <li>About</li>
+          <li>XP</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default Sidebar;
