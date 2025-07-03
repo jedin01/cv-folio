3f304c209bba62491c1d48328feebf3bbed55d42
abnerLouren <abnerjaredejede@gmail.com>
2025-07-03 01:48:16 +0100

    montando css da pagina home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
+// app/page.tsx ou app/layout.tsx
+
 import { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Home",
@@ -7,10 +10,34 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <main className="">
-      <div className="leftside-image"></div>
+    <main className="flex justify-between items-center h-screen bg-black text-white px-40">
+      {/* SVG com imagem clipada pela forma */}
+      <div className="">
+        <svg
+          width="334"
+          height="325"
+          viewBox="0 0 424 400"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <defs>
+            <clipPath id="formaClip">
+              <path d="M98 97V0H380V400H330V296H223V400H0V97H98Z" />
+            </clipPath>
+          </defs>
+
+          <image
+            href="/images/jed.jpg"
+            width="424"
+            height="400"
+            clipPath="url(#formaClip)"
+            preserveAspectRatio="xMidYMid slice"
+          />
+        </svg>
+      </div>
+
       <div className="rgihtside-content">
-        <div className="name">Abner Lourenço</div>
+        <div className="text-[60px]" >Abner Lourenço</div>
         <div className="slogan">"Tentando chegar ao modo criativo"</div>
         <div className="solutions">
           <div className="opens-source"></div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,11 @@
 export default function Sidebar({}) {
   return (
     <>
-      <nav className="bg-black text-white flex">
+      <nav className="bg-gray-800 text-white flex justify-between py-6 fixed w-full px-40">
         <div>
           <img src="" alt="logo" />
         </div>
-        <ul className="flex">
+        <ul className="flex gap-3">
           <li>Home</li>
           <li>About</li>
           <li>XP</li>
