375c74fb44b8db3fc32321df5238180690816538
abnerLouren <abnerjaredejede@gmail.com>
2025-07-10 02:35:49 +0100

    definindo forma de disposição dos elmentos na homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,10 +12,18 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center h-dvh">
+    <main className="flex flex-col items-center justify-center h-dvh">
       <Terminal />
-      <div className="w-[300px] h-[300px] bg-blue-400 rounded-[5px] z-[-2] relative top-[-150] left-[-120]">
-        <img src="" alt="" />
+
+      <div className="grid grid-cols-2 w-full text-[12px]">
+        <div className="h-fit w-fit flex flex-col gap-[10px] pt-[40px]">
+          <h1 className="border-b-[1px] w-fit font-bold py-[5px]" >Desenvolvedor de Software</h1>
+          <p className="w-[250px]">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut enim
+            velit qui voluptatem labore omnis hic beatae ratione!.
+          </p>
+        </div>
+        <div className="w-[300px] h-[300px] bg-white rounded-[5px] z-[2] relative top-[-150] left-[-120] overflow-hidden"></div>
       </div>
     </main>
   );
