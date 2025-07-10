f8cd1865f36e7b4850190ef0dc0ee179e9cf71bd
abnerLouren <abnerjaredejede@gmail.com>
2025-07-10 03:05:53 +0100

    donie

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Sidebar from "../components/Navbar";
+import Navbar from "../components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,8 +28,9 @@ export default function RootLayout({
         />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#04052e] text-white font-[Archivo_Black] px-50`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#04052e] text-white font-[Archivo_Black] w-dvw h-dvh`}
       >
+        <Navbar />
         {children}
       </body>
     </html>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,30 +1,12 @@
-export default function Sidebar() {
+export default function Navbar() {
   return (
     <>
-      <nav className=" text-white flex justify-between py-6 fixed w-full px-40 font-[Archivo]">
-        <div>
-          <img src="" alt="logo" />
-        </div>
-        <ul className="flex gap-3">
-          <li>
-            <a href="#home">Home</a>
-          </li>
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a
-              href="#xp"
-              className="transition-colors duration-300 hover:text-green-500"
-            >
-              XP
-            </a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
+      <div className="w-full flex justify-center fixed pt-[20px]" >
+        <nav className="bg-[#2c2c2c] border border-[#5a5a5f] w-[600px] h-[40px] rounded-[5px]">
+          <div className=""></div>
+          <div className=""></div>
+        </nav>
+      </div>
     </>
   );
 }
