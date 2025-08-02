922f860f42475bbb49f2c4cac86467074222f18d
abnerLouren <abnerjaredejede@gmail.com>
2025-08-02 20:41:43 +0100

    mudando aspecto dos icones

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,30 @@
-import { Code2Icon, House, PenTool } from "lucide-react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faDiagramProject,
+  faBriefcase,
+  faUser
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar() {
   return (
     <>
-      <div className="h-full flex justify-center fixed pt-[20px] top-[10%] right-5">
-        <nav className="w-fit h-fit rounded-[5px] overflow-hidden flex justify-center flex-col">
-          <div className="border-b-2 border-[#404049] h-fit w-full bg-[#2e2e3c]">
-            <div className="flex items-center m-2 p-2 gap-5 bg-[#404049] w-fit h-fit rounded-[6px]">
-              <div className="">
-                <Code2Icon />
-              </div>
-              <div className="">
-                <PenTool />
-              </div>
-            </div>
-          </div>
-          <div className="w-fit">
-            <div>
-              <House/>
-            </div>
-          </div>
-        </nav>
+      <div className=" bg-[#333331] p-4 rounded-[70px] fixed text-[#E7E9E8] top-[50vh] right-5 z-50 flex flex-col gap-10 overflow-hidden">
+        <a href="/home" className="hover:text-[#febe76] transition">
+          <FontAwesomeIcon icon={faHouse} className="w-4 h-4" />
+        </a>
+        <a href="/experiencia" className="hover:text-[#febe76] transition">
+          <FontAwesomeIcon icon={faDiagramProject} className="w-4 h-4" />
+        </a>
+        <a
+          href="/empresas_parcerias_projetos"
+          className="hover:text-[#febe76] transition"
+        >
+          <FontAwesomeIcon icon={faBriefcase} className="w-4 h-4" />
+        </a>
+        <a href="/sobre" className="hover:text-[#febe76] flex items-center gap-2">
+          <FontAwesomeIcon icon={faUser} className="w-4 h-4" />
+        </a>
       </div>
     </>
   );
