2e88911286e5b26eb88d896e4e6a9dfd47db31ad
abnerLouren <abnerjaredejede@gmail.com>
2025-08-02 12:04:16 +0100

    adicionando hero-pic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -123,7 +123,9 @@ export default function Home() {
         </p>
       </div>
       
-      <div className="bg-[#333331] w-[416px] h-[651px] rounded-[70px] text-white scale-[.78] absolute top-[-30] left-[90] z-0"></div>
+      <div className="bg-[#333331] w-[416px] h-[651px] rounded-[70px] text-white scale-[.78] absolute top-[-30] left-[90] z-0">
+        <img src="/images/hero-pic.png" />
+      </div>
       <div className="h-[1700px] w-[1200px] bg-[url('/images/fundo.svg')] bg-contain bg-center bg-no-repeat relative top-[-150] z-10"></div>
     </>
   );
