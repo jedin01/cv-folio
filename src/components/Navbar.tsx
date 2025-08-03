75fa43729720abcad9eef45bd3c2b25a5482e148
abnerLouren <abnerjaredejede@gmail.com>
2025-08-03 07:08:14 +0100

    adicionando botao para download do CV

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,65 +1,45 @@
-"use client";
-
-import { usePathname } from "next/navigation";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHouse,
   faDiagramProject,
   faUser,
+  faDownload
 } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar() {
-  const pathname = usePathname();
-  const activeLinkClass = "text-[#febe76]";
-
-  const getLinkClass = (path: string) => {
-    return pathname === path ? activeLinkClass : "";
-  };
-
   return (
     <>
-      <div className=" bg-[#333331] p-4 rounded-[70px] fixed text-[#E7E9E8] top-[50vh] right-5 z-50 flex flex-col gap-8 overflow-hidden">
+      <div className=" bg-[#333331] p-4 rounded-[70px] fixed text-[#E7E9E8] top-[50vh] right-5 z-50 flex flex-col gap-10 overflow-hidden">
         <a
           href="/"
           title="Home"
-          className={`hover:text-[#febe76] transition flex flex-col items-center gap-2 ${getLinkClass(
-            "/",
-          )}`}
+          className="hover:text-[#febe76] transition"
         >
           <FontAwesomeIcon icon={faHouse} className="w-4 h-4" />
-          <div
-            className={`w-1.5 h-1.5 rounded-full ${
-              pathname === "/" ? "bg-[#febe76]" : "bg-transparent"
-            }`}
-          ></div>
         </a>
         <a
           href="/xp"
           title="Experiência"
-          className={`hover:text-[#febe76] transition flex flex-col items-center gap-2 ${getLinkClass(
-            "/xp",
-          )}`}
+          className="hover:text-[#febe76] transition"
         >
           <FontAwesomeIcon icon={faDiagramProject} className="w-4 h-4" />
-          <div
-            className={`w-1.5 h-1.5 rounded-full ${
-              pathname === "/xp" ? "bg-[#febe76]" : "bg-transparent"
-            }`}
-          ></div>
         </a>
         <a
           href="/about"
-          title="Sobre"
-          className={`hover:text-[#febe76] transition flex flex-col items-center gap-2 ${getLinkClass(
-            "/about",
-          )}`}
+          title="Sobre mim"
+          className="hover:text-[#febe76] flex items-center gap-2"
         >
           <FontAwesomeIcon icon={faUser} className="w-4 h-4" />
-          <div
-            className={`w-1.5 h-1.5 rounded-full ${
-              pathname === "/about" ? "bg-[#febe76]" : "bg-transparent"
-            }`}
-          ></div>
+        </a>
+        <a
+          href="/cv.pdf" // Coloque o seu CV na pasta `public` e atualize este caminho se necessário
+          download
+          title="Download CV"
+          className="hover:text-[#febe76] transition-colors duration-300 flex flex-col items-center gap-2"
+        >
+          <FontAwesomeIcon icon={faDownload} className="w-4 h-4" />
+          {/* Este div vazio ajuda a manter o alinhamento consistente com os outros ícones */}
+          <div className="w-1.5 h-1.5"></div>
         </a>
       </div>
     </>
