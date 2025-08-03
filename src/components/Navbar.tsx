dbc94665199c2eae4f18b6d341656538738052cb
abnerLouren <abnerjaredejede@gmail.com>
2025-08-03 07:17:15 +0100

    acabando com erro d reconhecimento d ancoras em producao

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,35 +1,67 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHouse,
   faDiagramProject,
   faUser,
-  faDownload
+  faDownload,
 } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 
 export default function Navbar() {
+  const pathname = usePathname();
+  const activeLinkClass = "text-[#febe76]";
+
+  const getLinkClass = (path: string) => {
+    return pathname === path ? activeLinkClass : "";
+  };
+
   return (
     <>
-      <div className=" bg-[#333331] p-4 rounded-[70px] fixed text-[#E7E9E8] top-[50vh] right-5 z-50 flex flex-col gap-10 overflow-hidden">
-        <a
-          href="/"
-          title="Home"
-          className="hover:text-[#febe76] transition"
-        >
-          <FontAwesomeIcon icon={faHouse} className="w-4 h-4" />
-        </a>
+      <div className=" bg-[#333331] p-4 rounded-[70px] fixed text-[#E7E9E8] top-[50vh] right-5 z-50 flex flex-col gap-8 overflow-hidden">
+        <Link
+              href="/"
+              title="Home"
+              className={`hover:text-[#febe76] transition-colors duration-300 flex flex-col items-center gap-2 ${
+                pathname === '/' ? 'text-[#febe76]' : ''
+              }`}
+            >
+              <FontAwesomeIcon icon={faHouse} className="w-4 h-4" />
+              <div
+                className={`w-1.5 h-1.5 rounded-full transition-colors duration-300 ${
+                  pathname === '/' ? 'bg-[#febe76]' : 'bg-transparent'
+                }`}
+              ></div>
+            </Link>
         <a
           href="/xp"
           title="Experiência"
-          className="hover:text-[#febe76] transition"
+          className={`hover:text-[#febe76] transition-colors duration-300 flex flex-col items-center gap-2 ${getLinkClass(
+            "/xp",
+          )}`}
         >
           <FontAwesomeIcon icon={faDiagramProject} className="w-4 h-4" />
+          <div
+            className={`w-1.5 h-1.5 rounded-full transition-colors duration-300 ${
+              pathname === "/xp" ? "bg-[#febe76]" : "bg-transparent"
+            }`}
+          ></div>
         </a>
         <a
           href="/about"
-          title="Sobre mim"
-          className="hover:text-[#febe76] flex items-center gap-2"
+          title="Sobre"
+          className={`hover:text-[#febe76] transition-colors duration-300 flex flex-col items-center gap-2 ${getLinkClass(
+            "/about",
+          )}`}
         >
           <FontAwesomeIcon icon={faUser} className="w-4 h-4" />
+          <div
+            className={`w-1.5 h-1.5 rounded-full transition-colors duration-300 ${
+              pathname === "/about" ? "bg-[#febe76]" : "bg-transparent"
+            }`}
+          ></div>
         </a>
         <a
           href="/cv.pdf" // Coloque o seu CV na pasta `public` e atualize este caminho se necessário
