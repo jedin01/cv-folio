08a13212e0cd6d5ccedb41846ab899095b78a44d
abnerLouren <abnerjaredejede@gmail.com>
2025-08-04 04:41:26 +0100

    mudando elementos img - sgv+html

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "./styles/home.css";
 import Navbar from "../components/Navbar";
 
 const geistSans = Geist({
